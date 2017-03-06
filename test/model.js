7c5c2b1c9ddbbd4bb70b5acddac970c8c85ea674
Geoffrey Goodman <ggoodman@gmail.com>
2017-03-06 11:53:12 -0500

    Add support for module.webtask api

--- test/model.js
+++ test/model.js
@@ -12,49 +12,65 @@ const expect = Code.expect;
 
 
 describe('Sandbox programming model', () => {
-    
+
     const logger = {
         info: () => null,
         warn: () => null,
         error: () => null,
     };
-    
+
     it('will correctly respond to webtask_pb=1 in the query parameters', done => {
         const code = (ctx, cb) => cb(null, ctx.body);
         const payload = { hello: 'world' };
-        
+
         Runtime.simulate(code, { logger, parseBody: true, payload, method: 'POST' }, (res) => {
             expect(res.statusCode).to.equal(200);
             expect(res.payload).to.be.a.string();
-            
+
             const json = JSON.parse(res.payload);
-            
+
             expect(json).to.equal({ hello: 'world' });
-            
+
             done();
         });
     });
-    
+
     it('fails with malformed javascript', done => {
         const code = 'malformed javascript';
-        
+
         Runtime.simulate(code, { logger }, (res) => {
             expect(res.statusCode).to.equal(400);
             expect(res.payload).to.be.a.string().and.contain('Unable to compile submitted JavaScript');
-            
+
             done();
         });
     });
-    
+
     it('fails with requests that are too large', done => {
         const code = (ctx, cb) => cb(null, ctx.raw_body);
         const maxBodySize = 100;
         const payload = '0'.repeat(500 * 1024);
-        
+
         Runtime.simulate(code, { logger, maxBodySize, parseBody: true, payload, method: 'POST' }, (res) => {
             expect(res.statusCode).to.equal(400);
             expect(res.payload).to.be.a.string().and.contain('Script exceeds the size limit');
-            
+
+            done();
+        });
+    });
+
+    it('exposes secrets using the module.webtask api', done => {
+        const code = `
+            const secrets = module.webtask.secrets;
+
+            module.exports = (cb) => cb(null, { secrets });
+        `;
+        const secrets = { key: 'value' };
+
+        Runtime.simulate(code, { logger, secrets, parseBody: true }, (res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.payload).to.equal(JSON.stringify({ secrets }));
+
             done();
         });
     });
--- test/serving.js
+++ test/serving.js
@@ -13,76 +13,96 @@ const expect = Code.expect;
 
 
 lab.experiment('Local webtask server', () => {
-    
+
     const logger = {
         info: () => null,
         warn: () => null,
         error: () => null,
     };
     let server;
-    
+
     lab.afterEach(done => {
         server && server.listening
             ?   server.close(done)
             :   done();
     });
-    
+
     lab.test('will run an exported webtask (1-argument)', done => {
         const code = Fs.readFileSync(Path.join(__dirname, '..', 'fixtures', 'exports_hello_world_1.js'), 'utf8');
         server = Runtime.createServer(code, { logger });
-        
+
         server.listen(3001);
-        
+
         Request.get('http://localhost:3001', { json: false }, (err, res, body) => {
             server.close(done);
-            
+
             expect(err).to.be.null();
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('"hello world"');
         });
     });
-    
+
     lab.test('will run an exported webtask (2-argument)', done => {
         const code = Fs.readFileSync(Path.join(__dirname, '..', 'fixtures', 'exports_hello_world_2.js'), 'utf8');
         server = Runtime.createServer(code, { logger });
-        
+
         server.listen(3001);
-        
+
         Request.get('http://localhost:3001', { json: false }, (err, res, body) => {
             server.close(done);
-            
+
             expect(err).to.be.null();
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('"hello world"');
         });
     });
-    
+
     lab.test('will run an exported webtask (3-argument)', done => {
         const code = Fs.readFileSync(Path.join(__dirname, '..', 'fixtures', 'exports_hello_world_3.js'), 'utf8');
         server = Runtime.createServer(code, { logger });
-        
+
         server.listen(3001);
-        
+
         Request.get('http://localhost:3001', { json: false }, (err, res, body) => {
             server.close(done);
-            
+
             expect(err).to.be.null();
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal('"hello world"');
         });
     });
-    
-    lab.test('will only run a webtask once per request', done => {
-        let count = 0;
-        
-        const webtaskFn = (cb) => { count++; console.log('run'); cb(null, count); };
-        server = Runtime.createServer(webtaskFn, { logger });
-        
+
+    lab.test('supports the module.webtask api when serving webtasks', done => {
+        const code = `
+            const secrets = module.webtask.secrets;
+
+            module.exports = (cb) => cb(null, { secrets });
+        `;
+        const secrets = { key: 'value' };
+        server = Runtime.createServer(code, { logger, secrets });
+
         server.listen(3001);
-        
+
         Request.get('http://localhost:3001', { json: false }, (err, res, body) => {
             server.close(done);
-            
+
+            expect(err).to.be.null();
+            expect(res.statusCode).to.equal(200);
+            expect(body).to.equal(JSON.stringify({ secrets }));
+        });
+    });
+
+    lab.test('will only run a webtask once per request', done => {
+        let count = 0;
+
+        const webtaskFn = (cb) => { count++; console.log('run'); cb(null, count); };
+        server = Runtime.createServer(webtaskFn, { logger });
+
+        server.listen(3001);
+
+        Request.get('http://localhost:3001', { json: false }, (err, res, body) => {
+            server.close(done);
+
             expect(err).to.be.null();
             expect(res.statusCode).to.equal(200);
             expect(body).to.equal("1");
